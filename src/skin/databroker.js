29728894897176b5df088443deda7fbb70dbcfd9
Maggie <mingyaf@andrew.cmu.edu>
2020-12-01 17:43:47 -0800

    add data broker features

---
+++ src/skin/databroker.js
@@ -0,0 +1,43 @@
+'use strict';
+$(document).ready(function () {
+    
+    $('#submitemail').on("click" ,function() {
+        var emailadd = $("#input_email").val()
+        var ccpaheader =""
+        if($("#request_info").is(":checked")){
+            ccpaheader = ccpaheader.concat("1")
+        }
+        else{
+            ccpaheader = ccpaheader.concat("u")
+        }
+        if($("#request_delete").is(":checked")){
+            ccpaheader = ccpaheader.concat("1")
+        }
+        else{
+            ccpaheader = ccpaheader.concat("u")
+        }
+        if($("#nosell").is(":checked")){
+            ccpaheader = ccpaheader.concat("1")
+        }
+        else{
+            ccpaheader = ccpaheader.concat("u")
+        }
+        SendtoDatabrokers(ccpaheader,emailadd)
+        alert("Your request have been sent to data brokers in the data broker list")
+    });
+
+    function SendtoDatabrokers(ccpaheader,email){
+        $.ajax({
+            type:"Get",
+            url:"data-brokers.csv",
+            dataType:"text",
+            success:function(data){
+                var lines = data.split("\n")
+                lines.splice(0,1)
+                lines.forEach(item=>{
+                    $.ajax({method:"GET",url:item,data:{email: email},headers:{"ccpa1":ccpaheader}})
+                })
+            }
+        })
+    }
+});
